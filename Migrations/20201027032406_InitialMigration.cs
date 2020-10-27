f5a6c7b464f214ebec37a9b8ba3ffef6a02fe254
Gustavo Gomes <dev.gustavogomes@gmail.com>
2020-10-27 00:30:37 -0300

    Create Inital Migration

---
+++ Migrations/20201027032406_InitialMigration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Happy.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Orphanages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: false),
+                    Latitude = table.Column<double>(nullable: false),
+                    Longitutde = table.Column<double>(nullable: false),
+                    About = table.Column<string>(maxLength: 300, nullable: false),
+                    Instructions = table.Column<string>(nullable: false),
+                    Opening_hours = table.Column<string>(nullable: false),
+                    Open_on_weekends = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Orphanages", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Orphanages");
+        }
+    }
+}
--- Models/Orphanage.cs
+++ Models/Orphanage.cs
@@ -1,14 +1,32 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace Happy.Models
 {
   public class Orphanage
   {
     public int Id { get; set; }
+
+    [Required]
     public string Name { get; set; }
+
+    [Required]
     public double Latitude { get; set; }
+
+    [Required]
     public double Longitutde { get; set; }
+
+    [Required]
+    [MaxLength(300)]
     public string About { get; set; }
+
+    [Required]
     public string Instructions { get; set; }
+
+    [Required]
     public string Opening_hours { get; set; }
-    public bool Open_on_weekends { get; set; }
+
+    [Required]
+    public Boolean Open_on_weekends { get; set; }
   }
 }
