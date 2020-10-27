b033634c8dbd574c6053eed11bd1c5d404f0b3f8
Gustavo Gomes <dev.gustavogomes@gmail.com>
2020-10-26 23:36:05 -0300

    Create HappyDbContext

---
+++ Database/HappyDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+using Happy.Models;
+
+namespace Happy.Database
+{
+  public class HappyDbContext : DbContext 
+  {
+    public HappyDbContext(DbContextOptions<HappyDbContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Orphanage> Orphanages { get; set; }
+  }
+}
