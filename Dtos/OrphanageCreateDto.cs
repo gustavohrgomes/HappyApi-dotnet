91b0d55b51cfc545ec40e15704c2129ef6a6c022
Gustavo Gomes <dev.gustavogomes@gmail.com>
2020-10-28 00:39:37 -0300

    Add Create/Read Dtos

---
+++ Dtos/OrphanageCreateDto.cs
@@ -0,0 +1,21 @@
+namespace Happy.Dtos
+{
+  public class OrphanageCreateDto
+  {
+    public int Id { get; set; }
+    
+    public string Name { get; set; }
+
+    public double Latitude { get; set; }
+
+    public double Longitude { get; set; }
+
+    public string About { get; set; }
+
+    public string Instructions { get; set; }
+
+    public string Opening_hours { get; set; }
+
+    public bool Open_on_weekends { get; set; }
+  }
+}
---
+++ Dtos/OrphanageReadDto.cs
@@ -0,0 +1,20 @@
+namespace Happy.Dtos
+{
+  public class OrphanageReadDto
+  {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    
+    public double Latitude { get; set; }
+
+    public double Longitude { get; set; }
+    
+    public string About { get; set; }
+   
+    public string Instructions { get; set; }
+    
+    public string Opening_hours { get; set; }
+
+    public bool Open_on_weekends { get; set; }
+  }
+}
