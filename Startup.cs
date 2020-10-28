16bde590f021ffd021198973bbd7a6b39e954cdc
Gustavo Gomes <dev.gustavogomes@gmail.com>
2020-10-28 00:37:57 -0300

    Add & register automapper in Startup class

--- Startup.cs
+++ Startup.cs
@@ -11,49 +11,51 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 
 using Happy.Database;
-using Microsoft.EntityFrameworkCore;
+using AutoMapper;
 
 namespace Happy
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<HappyDbContext>(options => options.UseSqlite(
-                Configuration.GetConnectionString("HappyConnection")));
-
-            services.AddControllers();
-
-            //services.AddScoped<IOrphanageRepository, MockOrphanageRepository>();
-            services.AddScoped<IOrphanageRepository, SqlOrphanageRepository>();
-        }
-
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddDbContext<HappyDbContext>(options => options.UseSqlite(
+          Configuration.GetConnectionString("HappyConnection")));
+
+      services.AddControllers();
+
+      services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+      services.AddScoped<IOrphanageRepository, SqlOrphanageRepository>();
+    }
+
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseHttpsRedirection();
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
