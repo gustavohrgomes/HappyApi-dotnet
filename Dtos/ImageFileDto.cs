8f43bd97a6b13eb6d77a391110ca58b3e2948496
Gustavo Gomes <dev.gustavogomes@gmail.com>
2020-11-11 00:48:09 -0300

    Update ImageFileDto

--- Dtos/ImageFileDto.cs
+++ Dtos/ImageFileDto.cs
@@ -1,12 +1,15 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Http;
 
 namespace Happy.Dtos
 {
   public class ImageFileDto
   {
-    public string Id { get; set; }
+    public int Id { get; set; }
+
     public string Path { get; set; }
-    public int OrphanageId { get; set; }
+
+    
   }
 }
